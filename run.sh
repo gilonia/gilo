4049fad5bef06c4367eab7b60db7477d7d1a2849
gilonia <164995764+gilonia@users.noreply.github.com>
2024-03-26 14:07:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e8eb0a06-6f4a-4a51-aa65-b6020b13202b/)
 
 
 echo "$curl_output" >> Update.tsx
